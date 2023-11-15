d62b7af91ba859c19bceccc02e5cb492140075df
Shivam Patidar <125869590+shivampatidar7932@users.noreply.github.com>
2023-11-15 12:10:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const extendHex = (shortHex) => {
-  // write your code here
+    // Remove '#' if present and convert to uppercase
+    shortHex = shortHex.replace(/^#/, '').toUpperCase();
+
+    // Extend the short hex code to the full hex code
+    const fullHex = shortHex
+        .split('')
+        .map(component => component.repeat(2))
+        .join('');
+
+    // Add '#' prefix to the full hex code
+    return '#' + fullHex;
 };
 
 // Do not change the code below.
